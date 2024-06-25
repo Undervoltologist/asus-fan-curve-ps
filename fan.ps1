b1f89c00867c0186b089cdd4eb4969cc4a39e92d
akillisandalye <93976452+Undervoltologist@users.noreply.github.com>
2024-06-25 16:25:21 +0300

    Add files via upload

---
+++ fan.ps1
@@ -0,0 +1,71 @@
+# fan curve
+$tripPoints = @(
+    @{ temp = 40; speed = 0 },
+    @{ temp = 48; speed = 22 },
+    @{ temp = 55; speed = 40 },
+    @{ temp = 60; speed = 55 },
+    @{ temp = 77; speed = 80 },
+    @{ temp = 86; speed = 100 }
+)
+
+# getting CPU temp
+function Get-CPUTemp {
+    $output = .\AsusFanControl.exe --get-cpu-temp
+    if ($output -match "Current CPU temp: (\d+)") {
+        return [int]$matches[1]
+    }
+    return $null
+}
+
+# getting fan speed
+function Get-FanSpeeds {
+    $output = .\AsusFanControl.exe --get-fan-speeds
+    if ($output -match "Current fan speeds: (\d+) (\d+) RPM") {
+        return @([int]$matches[1], [int]$matches[2])
+    }
+    return $null
+}
+
+# setting fan speed
+function Set-FanSpeeds {
+    param ($speed)
+    .\AsusFanControl.exe --set-fan-speeds=$speed
+}
+
+# fan curve loop
+while ($true) {
+    # get the current CPU temp
+    $cpuTemp = Get-CPUTemp
+    if ($cpuTemp -eq $null) {
+        Write-Output "Failed to get CPU temperature."
+        Start-Sleep -Seconds 3
+        continue
+    }
+
+    # select the trip point depending on temp
+    $fanSpeed = 0
+    foreach ($tripPoint in $tripPoints) {
+        if ($cpuTemp -ge $tripPoint.temp) {
+            $fanSpeed = $tripPoint.speed
+        }
+    }
+
+    # clear console screen
+    Clear-Host
+
+    # get current fan RPM for output
+    $fanSpeeds = Get-FanSpeeds
+    if ($fanSpeeds -eq $null) {
+        Write-Output "Failed to get fan speeds."
+    } else {
+        Write-Output "Fan speeds: $($fanSpeeds[0]) RPM, $($fanSpeeds[1]) RPM"
+    }
+
+    # wait for 3 sec before the next check
+    Start-Sleep -Seconds 3
+
+    Set-FanSpeeds -speed $fanSpeed
+
+    # wait extra 2 secs so setting fan speed has a delay
+    Start-Sleep -Seconds 2
+}
